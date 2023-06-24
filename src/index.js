b4f66e91bdf00de1d38399a622ad010e13ae82ad
Cheung <402832626@qq.com>
2023-06-24 17:40:10 +0800

    react chatper 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(<h1>this a demo</h1>, document.getElementById("root"));
+
+function Hello() {
+    return <h1>Hello world123</h1>;
+}
+
+const element = <Hello />;
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+// Component;
+
+function Name() {
+    return <h1>姓名:Cheung</h1>;
+}
+
+function Age() {
+    return <h1>年龄:27</h1>;
+}
+
+function Style() {
+    return <h1>猛男</h1>;
+}
+
+ReactDOM.render(
+    <div>
+        <Name />
+        <Age />
+        <Style />
+    </div>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//class
+
+// class Index extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = { count: 0 };
+//     }
+
+//     render() {
+//         return <h1>count:{this.state.count}</h1>;
+//     }
+// }
+
+// ReactDOM.render(<Index />, document.getElementById("root"));
+
+// class Index extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { count: 0 };
+//         this.add = this.add.bind(this);
+//         this.inc = this.inc.bind(this);
+//     }
+//     add() {
+//         this.setState({ count: this.state.count + 1 });
+//     }
+
+//     inc() {
+//         this.setState({ count: this.state.count - 1 });
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <h1>{this.state.count}</h1>
+//                 <button onClick={this.add}>CLICK++</button>
+//                 <button onClick={this.inc}>CLICK-1</button>
+//             </>
+//         );
+//     }
+// }
+// ReactDOM.render(<Index />, document.getElementById("root"));
+
+class Index extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <>
+                <h2>i ma index(father) transfer soon data</h2>
+                <Demo value={123} />
+            </>
+        );
+    }
+}
+
+class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { indexValue: this.props.value };
+    }
+
+    render() {
+        return (
+            <>
+                <h1>
+                    this is my father give me data : {this.state.indexValue}{" "}
+                </h1>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
+
+function tick() {
+    const element = (
+        <div>
+            <h1>nowtime:</h1>
+            <h2>{new Date().getTime}</h2>
+        </div>
+    );
+
+    ReactDOM.render(element, document.getElementById("root"));
+}
